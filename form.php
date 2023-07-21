fab5a21ae43b08cd8345c17a04c287dc2bd767f5
ajmalshakeer <ajmalrozario2k17@gmail.com>
2023-07-21 16:21:21 +0530

    reg form updated code

--- form.php
+++ form.php
@@ -1,38 +1,17 @@
-<?php
-$pass = "12345";
-$password = $_POST["password"];
-if( $password == $pass){
-    $check_pass = "Correct Password";
-}
-else{
-    $check_pass = "Incorrect Password";
-}
-
-
-
-$radioVal = $_POST["gender"];;
-
-if($radioVal == "female")
-{
-    $gender = "Female";
-}
-else if ($radioVal == "male")
-{
-    $gender = "Male";
-}
-
-?>
-
-<h2> Form Handling</h2>
-<b>Student Name:</b> <?php echo $_POST["name"]; ?><br>
-Password: <?php echo $check_pass; ?><br>
-Address: <?php echo $_POST["address"]; ?><br>
-Gender: <?php echo $gender; ?><br>
-Skill Set: 
-
-<?php
-$sname = $_POST['skill'];
-foreach ($sname as $skill){ 
-    echo $skill;
-}
-?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Contact Form</title>
+</head>
+<body>
+    <h1>Thank You</h1>
+    <p>Here is the information you have submitted:</p>
+    <ol>
+        <li><em>Name:</em> <?php echo $_POST["name"]?></li>
+        <li><em>ROLL NO:</em> <?php echo $_POST["rollno"]?></li>
+        <li><em>AGE:</em> <?php echo $_POST["age"]?></li>
+        <li><em>DOB:</em> <?php echo $_POST["DOB"]?></li>
+    </ol>
+</body>
+</html>
